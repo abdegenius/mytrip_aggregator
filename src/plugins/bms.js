fae140988c5c6140f8869cbc760cab9bb125d963
abdegenius <abijahjohnnie@gmail.com>
2022-01-16 23:41:49 +0100

    Updates

--- src/plugins/bms.js
+++ src/plugins/bms.js
@@ -11,6 +11,8 @@ module.exports = fp(async function (fastify, opts) {
         let destination_state = payload.destination_state
         let trip_date = payload.trip_date
         let state_confirmation = 0
+        let states = BMS_STATES()
+        return states.data
         BMS_STATES().data.filter(row => {
             if(row.name == departure_state){
                 state_confirmation += 1;
