fd9e5a9955928b624c38208a7aabcf19bec89b5f
abdegenius <abijahjohnnie@gmail.com>
2022-01-16 23:30:12 +0100

    Updates

--- src/plugins/bms.js
+++ src/plugins/bms.js
@@ -12,13 +12,12 @@ module.exports = fp(async function (fastify, opts) {
             }
         })
         let states = BMS_STATES.data;
-        return states
         let departure_state = payload.departure_state
         let destination_state = payload.destination_state
         let trip_date = payload.trip_date
         let departure_state_terminals = states.find(row => row.name == departure_state)
         let destination_state_terminals  = states.find(row => row.name == destination_state)
-
+        return departure_state_terminals
         if(destination_state_terminals.length > 0 && departure_state_terminals.length > 0){
             const CHECK_TRIP = await axios.get(api+`check_trip`,
             {
