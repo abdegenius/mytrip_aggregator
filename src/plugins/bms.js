18dea072f30620d9ff966f2913aa3e3b7790612d
abdegenius <abijahjohnnie@gmail.com>
2022-01-21 00:45:55 +0100

    Updates

--- src/plugins/bms.js
+++ src/plugins/bms.js
@@ -117,6 +117,7 @@ module.exports = fp(async function (fastify, opts) {
             }
         })
         let result = BOOK_TRIP.data.data
+        return result
         let vehicle = result.message.vehicle;
         let departure = result.message.departure_terminal.substring(0,-3);
         let destination = result.message.destination_terminal.substring(0,-3);
