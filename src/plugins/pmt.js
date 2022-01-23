d2b782b9423e566bdb1aeffefff98f5dbed3c06b
abdegenius <johnnieabijah2000@gmail.com>
2022-01-23 11:13:40 +0100

    Updates

--- src/plugins/pmt.js
+++ src/plugins/pmt.js
@@ -143,7 +143,7 @@ module.exports = fp(async function (fastify, opts) {
                 primary = passenger
             }
         })
-        let primary_name = (primary.name+" ");
+        let primary_name = (primary.name+" "+primary.name);
         primary_name.split(" ")
         const SAVE_PASSENGERS = await axios.post(api+`/pmt/passengers`, {
             "email": primary.email,
