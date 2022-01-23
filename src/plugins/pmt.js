57db01e5aa8075cdbde927c7f3c050f7d157f9e8
abdegenius <johnnieabijah2000@gmail.com>
2022-01-23 12:13:16 +0100

    Updates

--- src/plugins/pmt.js
+++ src/plugins/pmt.js
@@ -248,7 +248,8 @@ module.exports = fp(async function (fastify, opts) {
                                         "departure_terminal": terminal_from.name,
                                         "destination_terminal":  terminal_to.name,
                                         "seat_details": payload.passengers,
-                                        "provider": "PMT"
+                                        "provider": "PMT",
+                                        "payload": booking
                                     }
                                 ]
                             }
